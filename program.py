77d238439b1fb91e78740dbcb3b7c0a88223b3ad
Julius Edvardsson <juliusensen@gmail.com>
2021-08-23 20:21:00 +0200

    Added min, max, default to applicable arguments in help message

--- program.py
+++ program.py
@@ -7,13 +7,13 @@ import argparse, sys
 
 parser = argparse.ArgumentParser(description=__description__)
 parser.add_argument('--speed',            '-sp', nargs=1,   type=float, default=[0.225],
-                    help='speed when moving a window')
+                    help='speed when moving a window (min: 0, max: inf, default: 0.225)')
 
 parser.add_argument('--smoothing-factor', '-sm', nargs=1,   type=float, default=[0.85],
-                    help='interpolation factor when moving a window')
+                    help='interpolation factor when moving a window (min: 0, max: 1, default: 0.85)')
 
 parser.add_argument('--refresh-rate',     '-r',  nargs=1,   type=float, default=[60.0],
-                    help='number of updates per second')
+                    help='number of updates per second (min: 0, max: inf, default: 60.0)')
 
 parser.add_argument('--blacklist',        '-b',  nargs='+', type=str,   default=[],
                     help='blacklisted executable paths')
